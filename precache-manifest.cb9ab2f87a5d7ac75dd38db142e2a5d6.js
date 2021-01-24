ba6090cbf6a4f00650794c220a3b47cd460be87d
adharshr4 <adharshr4@gmail.com>
2021-01-24 15:15:11 +0530

    Updates

---
+++ precache-manifest.cb9ab2f87a5d7ac75dd38db142e2a5d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab799824f0a98a1373e0eb7a44e4a77",
+    "url": "/R-Amaon-Clone/index.html"
+  },
+  {
+    "revision": "aeafd632e7d78c83ecc2",
+    "url": "/R-Amaon-Clone/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "7ab7ba8a20059e5c5758",
+    "url": "/R-Amaon-Clone/static/js/2.ecad63df.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/R-Amaon-Clone/static/js/2.ecad63df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeafd632e7d78c83ecc2",
+    "url": "/R-Amaon-Clone/static/js/main.4fda30f2.chunk.js"
+  },
+  {
+    "revision": "e6b7a33637702ca7dba7",
+    "url": "/R-Amaon-Clone/static/js/runtime-main.445adb13.js"
+  }
+]);
